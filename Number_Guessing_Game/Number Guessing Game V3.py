db7515b79c514980937007199bc40743f058c3c8
redgrb246 <116602808+redgrb246@users.noreply.github.com>
2023-01-13 15:00:37 -0500

    Update Number Guessing Game V3.py

--- Number_Guessing_Game/Number Guessing Game V3.py
+++ Number_Guessing_Game/Number Guessing Game V3.py
@@ -7,15 +7,18 @@ while True:
     Repeat = 0
     while True:
         Repeat = 0 
+        #asking the user name this will not be used out side of this code there in no leaderboard
+        #if you get not a valid name somthing is broken
         while True:    
             try:    
                 name = str(input("What is your name?:  "))
                 break
             except ValueError:
-                print("Not A Vailid Name")
+                print("Not A Valid Name")
                 continue
         print("Hello "+ name +" \nWould you like to play a game?")
         print("Type 1 to continue \nType 2 to stop")   
+        #asking the user if they realy want to play
         while True:
             try:
                 Repeat = int(input('Enter Number 1 or 2: '))
@@ -34,7 +37,8 @@ while True:
     elif Repeat == 2:
         break
     while True:    
-        while True:    
+        while True: 
+            #asking for range values then checking to see if they are valid
             try:    
                 range1 = int(input("Range Number 1:  "))
                 break
@@ -51,6 +55,7 @@ while True:
 
         #defing the code
         def code():
+            #setting the guess_taken to 1 to fix a bug
             guess_taken = 1  
             while True:  
                 while True:
@@ -61,7 +66,7 @@ while True:
                         print("Not A Valid Number")
                         continue
                 Guess2 = str(Guess)
-                #shows how many guesse it took to get the answer
+                #shows how many guess it took to get the answer and checking to make sure that the input is vaiid
                 guess_taken2 = str(guess_taken) 
                 if Guess < num and Guess >= range1:
                     print("The Number Is More Than  " + Guess2) 
@@ -84,6 +89,7 @@ while True:
                 code()
                 
             print(name +" Would You Like To Play Again? \nType 1 To Continue \nType 2 to Stop")
+            #asking user if they want to repeat and checking to see if the number is valid
             while True:
                 try:
                     Repeat = int(input('Enter Number 1 or 2: '))
@@ -104,8 +110,3 @@ while True:
         continue
     elif Repeat == 2:
         break
-    #asking user if they want to repeat    
-    
-    #repeating code
-    
-    
